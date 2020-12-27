eb6a4dd6b03c5228ec9f13503fec0d9fc03a3ee5
tilekbergen <tilekbergen.third@gmail.com>
2020-12-27 16:09:20 +0600

    initial commit

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ hamburger.addEventListener("click", () => {
     links.classList.toggle("fade");
     hamburger.classList.toggle("toggle")
 })
+
+//cursor
+
+const cursor = document.querySelector(".cursor")
+
+document.addEventListener("mousemove", (e) => {
+    cursor.setAttribute('style', 'top: ' +(e.pageY - 10)+ 'px; left: '+(e.pageX - 10)+'px;')
+})
